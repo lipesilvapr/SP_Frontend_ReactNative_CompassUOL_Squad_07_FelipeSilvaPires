102ca829c531e80737b234e9f78d6fdd7cfc7eee
Felipe Silva <Taxistaloko14@outlook.com>
2023-08-19 20:14:23 -0300

    basic-complete

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,8 @@
 "use strict";
 //nav bar
+const hamburguer = document.querySelector(".burger");
+const navMenu = document.querySelector(".nav-menu");
+hamburguer.addEventListener("click", () => {
+    hamburguer.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,8 @@
 //nav bar
+const hamburguer = document.querySelector(".burger")! as HTMLElement;
+const navMenu = document.querySelector(".nav-menu")! as HTMLElement;
 
+hamburguer.addEventListener("click", () => {
+  hamburguer.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
